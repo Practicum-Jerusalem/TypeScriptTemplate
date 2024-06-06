6a4537164a3639d404bc7a25545f186d09bd12be
AviadDerli <aviad@workin.co.il>
2024-06-06 15:53:08 +0300

    layout ts

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -1,5 +1,6 @@
 import {Request, Response, Router} from 'express'
 import UserService from '../services/UserService'
+import { Mapper } from '../helpers/Mapper'
 const router = Router()
 
 // CRUD : all users, signle user, create user, update user
@@ -7,6 +8,17 @@ router.get('/',async (req : Request, res: Response)=>{
     try{
         res.send(UserService.getAllUsers())
         // res.send(new UserService().getAllUsers())
+    }
+    catch(err){
+        res.status(400).send(err)
+    }
+})
+
+router.post('/',async (req : Request, res: Response)=>{
+    try{
+        let newBody = Mapper<CreateNewUserRequest>(new CreateNewUserRequest(),req.body)
+
+        res.send(UserService.createUser(newBody))
     }
     catch(err){
         res.status(400).send(err)
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,25 +1,41 @@
+import { SaveOptions, Document } from "mongoose";
 import UserController from "../controller/UserController";
+import IUser from "../interfaces/IUser";
 
 class UserService {
 
-    controller: UserController = new UserController()
+    private static controller: UserController = new UserController()
 
 
     static getAllUsers() {
         // return users;
     }
 
-    static getSingleUser(email) {
+    static async getSingleUser(email: string) {
+
+        return await this.controller.readOne({ email })
 
     }
-    static checkIfEmailExist(email) {
+    // static checkIfEmailExist(email) {
 
-    }
+    // }
 
-    static createUser(user) {
+    // req.body
+    static async createUser(body: CreateNewUserRequest) {
+        let oldUser = await this.getSingleUser(body.email)
+        if (oldUser) throw new Error("user is exist")
 
+            let nUser: IUser = {
+            email: body.email,
+            fullName: body.fName,
+            permission: "user",
+            password: String(Math.random() * 100 * 100),
+        }
+        let newUser = await this.controller.create(nUser)
+        if (newUser.save) newUser.save()
+
+        return newUser
     }
 }
-
 
 export default UserService
