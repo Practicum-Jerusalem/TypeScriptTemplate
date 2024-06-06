9958ba45715d69d0b6e471da8933b6a0bdc6c46d
AviadDerli <aviad@workin.co.il>
2024-06-06 16:22:28 +0300

    layout ts

--- src/interfaces/IController.ts
+++ src/interfaces/IController.ts
@@ -2,8 +2,8 @@ import { FilterQuery, UpdateQuery } from "mongoose"
 
 export default interface IController <T> {
     create(data : T): Promise<T>
-    read(filter: FilterQuery<T>): T[]
+    read(filter: FilterQuery<T>): Promise<T[]>
     readOne(filter: FilterQuery<T>): Promise<T | null>
-    update(filter: FilterQuery<T>, newData:UpdateQuery<T>): T
+    update(filter: FilterQuery<T>, newData:UpdateQuery<T>): Promise<T>
     del(filter: FilterQuery<T>): boolean
 }
